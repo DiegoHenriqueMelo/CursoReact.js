8a9055b0abcf9dc4f48cb9d84d557bf9620fa8b8
Diego Melo <diegohenriquemelo14@gmail.com>
2024-11-07 15:01:19 -0300

    Criando todos os componentes do REACT para a plicação

--- react-com-CLI/gitfind/src/pages/home/index.js
+++ react-com-CLI/gitfind/src/pages/home/index.js
@@ -1,5 +1,42 @@
+import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
+import "./styles.css";
+import ItenList from "../../components/ItenList"
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="" backgound app />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+          </div>
+          <div className="perfil">
+            <img
+              src="https://avatars.githubusercontent.com/u/140443998?v=4"
+              className="profile"
+            />
+            <div>
+              <h3>Diego Henrique Melo</h3>
+              <span>@diegohenriquemelo</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+        <hr/>
+        <div className="repositorios">
+          <h4>Repositórios</h4>
+          <ItenList  title="teste1" description="Teste de descrição"/>
+          <ItenList  title="teste1" description="Teste de descrição"/>
+          <ItenList  title="teste1" description="Teste de descrição"/>
+          <ItenList  title="teste1" description="Teste de descrição"/>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
